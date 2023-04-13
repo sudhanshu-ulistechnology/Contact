52c72095aa097ce126ccc231a952c9dc1841f640
sudhanshu-ulistechnology <sudhanshu.n@ulistechnology.com>
2023-04-13 11:54:07 +0530

    change

--- scr/Screen/contactList.js
+++ scr/Screen/contactList.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const contactList = () => {
   return (
     <View>
       <Text>contactList</Text>
     </View>
-  )
-}
+  );
+};
 
-export default contactList
+export default contactList;
