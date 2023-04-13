503ed0c60383c080972b2b0d408f1d94da7e67cc
sudhanshu-ulistechnology <sudhanshu.n@ulistechnology.com>
2023-04-13 11:28:48 +0530

    add screen

---
+++ scr/Screen/contactList.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const contactList = () => {
+  return (
+    <View>
+      <Text>contactList</Text>
+    </View>
+  )
+}
+
+export default contactList
