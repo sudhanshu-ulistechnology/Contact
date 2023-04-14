3bdb493797205bd80a150e375b5e69e2de7d5015
sudhanshu-ulistechnology <sudhanshu.n@ulistechnology.com>
2023-04-14 16:28:44 +0530

    update

--- scr/Screen/DetailContact.js
+++ scr/Screen/DetailContact.js
@@ -7,7 +7,7 @@ import { Text, View, Image, TouchableOpacity, StyleSheet, Alert, Modal } from 'r
 import { useState } from 'react';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 
 const DetailContact = ({ navigation, route }) => {
@@ -16,7 +16,7 @@ const DetailContact = ({ navigation, route }) => {
     const [modalVisibleShare, setModalVisibleShare] = useState(false);
     return (
         <View style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
-            
+
             <View>
                 <View style={{
                     backgroundColor: 'white',
@@ -33,8 +33,10 @@ const DetailContact = ({ navigation, route }) => {
                     </View>
                     <Text style={styles.profileData}>
                         {firstName} {maidenName} {lastName}</Text>
-                    <Text style={{ marginTop: 5 }}>Mobile {phone}</Text>
-
+                        <View style={{ flexDirection:'row' }}>
+                    <Text style={{ marginTop: 3, color:'#000'}}>Mobile</Text>
+                    <Text style={{marginTop:2, marginLeft:15, fontWeight:'bold', color:'#000'}}>{phone}</Text>
+                        </View>
                     <View style={{
                         flexDirection: 'row',
                         justifyContent: 'space-between', marginTop: 25,
@@ -76,15 +78,17 @@ const DetailContact = ({ navigation, route }) => {
                     borderColor: '#ccc',
                     marginTop: 20,
                     height: hp('9%'),
-                    width: wp('100%') 
+                    width: wp('100%')
                 }}>
                     <Text
                         style={{ marginTop: -15, fontSize: RFPercentage(1.5), color: '#000' }}
                     >Email</Text>
-                    <View style={{ flexDirection: 'row', }}>
+                    <View style={{ flex: 1, flexDirection: 'row', }}>
                         <Text style={{ fontSize: RFPercentage(2), fontWeight: 'bold', color: '#000' }}>{email}</Text>
-                        <View style={{ flex:1, marginStart: 170, height: hp('9%'),
-                    width: wp('100%') }}>
+                        <View style={{
+                            flex: 1, marginStart: 170, height: hp('9%'),
+                            width: wp('100%')
+                        }}>
                             <Icon name="email" size={24} color="grey" />
                         </View>
                     </View>
@@ -119,129 +123,117 @@ const DetailContact = ({ navigation, route }) => {
                 </View>
             </View>
             <View>
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={modalVisible}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-        setModalVisible(!modalVisible);
-        // setModalVisible(false)
-        setModalVisible(true);
-      }}>
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          <Text style={styles.modalTitleText}>Alert</Text>
-          <Text style={styles.modalMessageText}>Do you To Edit Contact?</Text>
-          <View style={styles.insideButton}>
-          <View>
-          <TouchableOpacity
-            style={[styles.button, styles.buttonClose]}
-            onPress={() => setModalVisible(!modalVisible)}>
-            <Text style={styles.textStyleCancel}>Cancel</Text>
-          </TouchableOpacity>
-          </View>
-          <View>
-          <TouchableOpacity
-          
-            style={[styles.button, styles.buttonContinue]}
-            onPress={() => {
-              navigation.navigate('Edit');
-            }}
-            >
-            <Text style={styles.textStyleCancel}>Continue</Text>
-          </TouchableOpacity>
-          </View>
-          </View>
-        </View>
-      </View>
-    </Modal>
-    
-    <View>
-    
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={modalVisibleShare}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-        setModalVisibleShare(!modalVisibleShare);
-        // setModalVisible(false)
-        setModalVisibleShare(true);
-      }}>
-      <View style={styles.centeredView}>
-        <View style={styles.modalView}>
-          {/* <Text style={styles.modalTitleText}>Alert</Text> */}
-          <Text style={styles.modalMessageText}>Comming Soon</Text>
-          <View style={styles.insideButton}>
-          <View>
-          <TouchableOpacity
-            style={[styles.button, styles.buttonClose,]}
-            onPress={() => setModalVisibleShare(!modalVisibleShare)}>
-            <Text style={styles.textStyleCancel}>Cancel</Text>
-          </TouchableOpacity>
-          </View>
-          <View>
-          {/* <TouchableOpacity
-          
-            style={[styles.button, styles.buttonContinue]}
-            // onPress={() => {
-            //   navigation.navigate('Second', { first_name, last_name, email, avatar });
-            //   // ('One', { item })
-            // }}
-            >
-            <Text style={styles.textStyleCancel}>Continue</Text>
-          </TouchableOpacity> */}
-          </View>
-          </View>
-        </View>
-      </View>
-    </Modal>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                        setModalVisible(!modalVisible);
+                        // setModalVisible(false)
+                        setModalVisible(true);
+                    }}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalTitleText}>Alert</Text>
+                            <Text style={styles.modalMessageText}>Do you To Edit Contact?</Text>
+                            <View style={styles.insideButton}>
+                                <View>
+                                    <TouchableOpacity
+                                        style={[styles.button, styles.buttonClose]}
+                                        onPress={() => setModalVisible(!modalVisible)}>
+                                        <Text style={styles.textStyleCancel}>Cancel</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <View>
+                                    <TouchableOpacity
 
-            <View style={{flex:1}}>
-            <View style={{
-                // flex:2,
-                flexDirection: 'row',
-                margin: 25,
-                justifyContent: 'space-between',
-                marginBottom:-50,
-                marginVertical: 140, 
-                height: hp('4%'),
-                width: wp('88%')
-            }}>
-                <TouchableOpacity
-                    onPress={() => { }}
-                    style={styles.bottomIcon}
-                >
-                    <Icon name="star-outline" size={26} color="black" />
-                    <Text>Favourites</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => setModalVisible(true)}
-                    style={styles.bottomIcon}
-                >
-                    <Icon name="edit" size={26} color="black" />
-                    <Text>Edit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => setModalVisibleShare(true)}
-                    style={styles.bottomIcon}
-                >
-                    <Icon name="share" size={26} color="black" />
-                    <Text>Share</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => { }}
-                    style={styles.bottomIcon}
-                >
-                    <Icon name="more-vert" size={26} color="black" />
-                    <Text>More</Text>
-                </TouchableOpacity>
-            </View>
+                                        style={[styles.button, styles.buttonContinue]}
+                                        onPress={() => {
+                                            navigation.navigate('Edit');
+                                        }}
+                                    >
+                                        <Text style={styles.textStyleCancel}>Continue</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
 
+                <View>
+
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisibleShare}
+                        onRequestClose={() => {
+                            Alert.alert('Modal has been closed.');
+                            setModalVisibleShare(!modalVisibleShare);
+                            // setModalVisible(false)
+                            setModalVisibleShare(true);
+                        }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                {/* <Text style={styles.modalTitleText}>Alert</Text> */}
+                                <Text style={styles.modalMessageText}>Comming Soon</Text>
+                                <View style={styles.insideButton}>
+                                    <View>
+                                        <TouchableOpacity
+                                            style={[styles.button, styles.ShareCancleButton]}
+                                            onPress={() => setModalVisibleShare(!modalVisibleShare)}>
+                                            <Text style={styles.textStyleCancel}>Cancel</Text>
+                                        </TouchableOpacity>
+                                    </View>
+
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
+
+                    <View style={{ flex: 1 }}>
+                        <View style={{
+                            // flex:2,
+                            flexDirection: 'row',
+                            margin: 25,
+                            justifyContent: 'space-between',
+                            marginBottom: -50,
+                            marginVertical: 140,
+                            height: hp('4%'),
+                            width: wp('88%')
+                        }}>
+                            <TouchableOpacity
+                                onPress={() => { }}
+                                style={styles.bottomIcon}
+                            >
+                                <Icon name="star-outline" size={26} color="black" />
+                                <Text>Favourites</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => setModalVisible(true)}
+                                style={styles.bottomIcon}
+                            >
+                                <Icon name="edit" size={26} color="black" />
+                                <Text>Edit</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => setModalVisibleShare(true)}
+                                style={styles.bottomIcon}
+                            >
+                                <Icon name="share" size={26} color="black" />
+                                <Text>Share</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={() => { }}
+                                style={styles.bottomIcon}
+                            >
+                                <Icon name="more-vert" size={26} color="black" />
+                                <Text>More</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
             </View>
-            </View>
-  </View>
         </View>
     );
 };
@@ -266,16 +258,16 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         borderWidth: 0.1,
     },
-    topIcon:{
+    topIcon: {
         marginHorizontal: 30,
     },
     bottomIcon: {
-        // flex:1,
+        flex: 1,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
-    
+
 
 
     centeredView: {
@@ -283,72 +275,76 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 22,
-      },
-      modalView: {
+    },
+    modalView: {
         backgroundColor: 'white',
         borderRadius: 20,
         padding: 30,
-        paddingBottom:2,
-        paddingVertical:30,
-        paddingHorizontal:30,
+        paddingBottom: 2,
+        paddingVertical: 30,
+        paddingHorizontal: 30,
         alignItems: 'center',
         shadowColor: '#000',
         shadowOffset: {
-          width: 0,
-          height: 2,
+            width: 0,
+            height: 2,
         },
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
-      },
+    },
 
-      buttonOpen: {
-        marginTop:100,
-      },
-      buttonClose: {
-        marginTop:8,
+    buttonOpen: {
+        marginTop: 100,
+    },
+    ShareCancleButton:{
+        alignSelf:'center',
+        paddingBottom:15,
+    },
+    buttonClose: {
+        marginTop: 8,
         // alignContent:'center',
-        
-        borderRadius:10,
-        padding:8,
-        marginRight:60,
-      },
-      modalMessageText:{
+
+        borderRadius: 10,
+        padding: 8,
+        marginRight: 60,
+    },
+    modalMessageText: {
         color: 'black',
         fontWeight: 'bold',
-      },
-      buttonContinue:{
-        marginTop:8,
-        
-        borderRadius:10,
-        padding:8,
-      },
-      textStyle: {
+    },
+    buttonContinue: {
+        marginTop: 8,
+
+        borderRadius: 10,
+        padding: 8,
+    },
+    textStyle: {
         color: 'black',
-        marginBottom:10,
-        fontSize:25,
+        marginBottom: 10,
+        fontSize: 25,
         fontWeight: 'bold',
         textAlign: 'center',
-      },
-      modalTitleText: {
+    },
+    modalTitleText: {
         marginBottom: 15,
-        marginTop:-28,
+        marginTop: -28,
         textAlign: 'center',
-        fontSize:18,
+        fontSize: 18,
         fontWeight: 'bold',
         color: 'black',
-      },
-      textStyleCancel:{
+    },
+    textStyleCancel: {
         color: '#2196F3',
-        fontSize:15,
+        fontSize: 15,
         fontWeight: 'bold',
         textAlign: 'center',
-      },
-      insideButton:{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-evenly',
-            marginTop:30,
-      },
+    },
+    insideButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginTop: 30,
+    },
 
 });
